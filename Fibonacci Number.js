2447391daa655f71b9e566a7300bf36c10c4677f
NishantPriyadarshi60 <nishantpriyadarshi60@gmail.com>
2023-11-22 20:09:56 +0530

    fibbonacci number

---
+++ Fibonacci Number.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/fibonacci-number/description/
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+
+    return fib(n - 1) + fib(n - 2)
+};
