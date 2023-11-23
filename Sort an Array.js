0e86db45cca847adb615a70b7ba845bebd5ff013
NishantPriyadarshi60 <nishantpriyadarshi60@gmail.com>
2023-11-23 08:13:15 +0530

    sort an array

---
+++ Sort an Array.js
@@ -0,0 +1,41 @@
+//  https://leetcode.com/problems/sort-an-array/description/
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+function merge(array1, array2) {
+    let combined = [];
+    let i = 0;
+    let j = 0;
+    while (i < array1.length && j < array2.length) {
+        if (array1[i] < array2[j]) {
+            combined.push(array1[i])
+            i++;
+        } else {
+            combined.push(array2[j])
+            j++;
+        }
+    }
+
+    while (i < array1.length) {
+        combined.push(array1[i])
+        i++;
+    }
+    while (j < array2.length) {
+        combined.push(array2[j])
+        j++;
+    }
+    return combined;
+}
+
+var sortArray = function (nums) {
+    if (nums.length === 1) return nums;
+    let mid = Math.floor(nums.length / 2);
+    let left = nums.slice(0, mid);
+    let right = nums.slice(mid);
+    return merge(sortArray(left), sortArray(right))
+};
