4cbe9df44673857801177ec514b84e86aa0444ca
NishantPriyadarshi60 <nishantpriyadarshi60@gmail.com>
2023-12-30 08:32:57 +0530

    counter

---
+++ Counter.js
@@ -0,0 +1,23 @@
+//  https://leetcode.com/problems/counter/
+
+
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+
+    let count = n;
+    return function () {
+        return count++;
+
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
