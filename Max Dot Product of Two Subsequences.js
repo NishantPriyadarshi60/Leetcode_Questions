358da7ced06a98909d53e844cb2fcf8f444d8335
NishantPriyadarshi60 <nishantpriyadarshi60@gmail.com>
2024-01-09 08:31:57 +0530

    Max Dot Product of Two Subsequences

---
+++ Max Dot Product of Two Subsequences.js
@@ -0,0 +1,29 @@
+//  https://leetcode.com/problems/max-dot-product-of-two-subsequences/description/
+
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDotProduct = function (nums1, nums2) {
+    const m = nums1.length;
+    const n = nums2.length;
+
+    if (m < n) {
+        return maxDotProduct(nums2, nums1);
+    }
+
+    const dp = new Array(n + 1).fill(Number.NEGATIVE_INFINITY);
+
+    for (let i = 0; i < m; i++) {
+        let prev = 0;
+        for (let j = 0; j < n; j++) {
+            const temp = dp[j + 1];
+            dp[j + 1] = Math.max(prev + nums1[i] * nums2[j], nums1[i] * nums2[j], dp[j], dp[j + 1]);
+            prev = temp;
+        }
+    }
+    return dp[n]
+};
