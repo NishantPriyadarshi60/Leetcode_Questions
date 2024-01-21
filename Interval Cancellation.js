60e7f8150335af308b33beb2b7723a9682e1829e
NishantPriyadarshi60 <nishantpriyadarshi60@gmail.com>
2024-01-21 23:21:58 +0530

    interval cancel

---
+++ Interval Cancellation.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/interval-cancellation/description/
+
+
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+    fn(...args);
+    let timer = setInterval(() => fn(...args), t);
+
+    let cancelFn = () => clearInterval(timer)
+    return cancelFn;
+};
