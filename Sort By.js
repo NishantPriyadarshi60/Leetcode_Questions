bbdba8cf50b4e886d27c13493fd9b2fe7c2934e0
NishantPriyadarshi60 <nishantpriyadarshi60@gmail.com>
2024-01-31 22:04:22 +0530

    sortBy

---
+++ Sort By.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sort-by/description/
+
+
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b));
+};ActiveXObject
